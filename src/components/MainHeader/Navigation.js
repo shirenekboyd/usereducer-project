a03437e490bdaff57b5019d8bc9c1a4504cb292f
Shirene Boyd <shirenekadkhodai@gmail.com>
2023-04-21 12:03:40 -0500

    added a store directory and auth-context.js file in order to break up prop chains for isLoggedIn, managing state behind the scenes from parent to all its children components and finally refactored the Navigation component to use useContext hook to pull in the AuthContext object

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import classes from './Navigation.module.css';
+import classes from "./Navigation.module.css";
+import AuthContext from "../../store/auth-context";
 
 const Navigation = (props) => {
+  //a more eligant way to pull AuthContext data in using a useContext hook as opposed to the AuthContext component in JSX
+  const ctx = useContext(AuthContext);
+
   return (
+    // <AuthContext.Consumer>
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctx.isLoggedIn && (
           <li>
             <button onClick={props.onLogout}>Logout</button>
           </li>
@@ -24,6 +29,7 @@ const Navigation = (props) => {
       </ul>
     </nav>
   );
+  // </AuthContext.Consumer>
 };
 
 export default Navigation;
---
+++ src/store/auth-context.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AuthContext = React.createContext({
+    isLoggedIn: false
+});
+
+export default AuthContext;
+
+//not a component that's why the file name is lower case
+
